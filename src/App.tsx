657928933b0ea13e6730bdfacc94ad86cef2b153
emaliikhokhar <bitf18a024@pucit.edu.pk>
2021-10-13 17:42:56 +0500

    Weather Cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,39 @@ import './App.css';
 import WeatherComponent from './Component/WeatherComponent';
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'weather-icons/css/weather-icons.css'
+import WeatherCards from './Component/WeatherCards';
 
 //API Key
-const API_Key: string = "f5bdf3f34a8b21ddb038918c128e2ccb"
+const API_Key: string = "f3a63dd6c483452c9fc61549211310"
 
 const App = () => {
-  const [data, setData] = useState(null);
+
+  // const [data, setData] = useState();
+  const cities: string[] = ["London", "Lahore", "Delhi", "Istanbul", "Paris"]
+  const [citiesState] = useState(cities);
 
   useEffect(() => {
     let response;
     async function FetchMyAPI() {
-      response = await fetch(`api.openweathermap.org/data/2.5/weather?q=London&appid=${API_Key}`)
-      console.log(response)
-    }
-    FetchMyAPI();
-  })
-
-  // options:Object = {
-  //   url: `api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_Key}`,
-  //   method: 'POST',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/json'
-  //   }
-  // };
+        response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_Key}&q=London&aqi=yes`);
+        response = await response.json();
+      }
+      FetchMyAPI();
+    })
 
     return (
       <div className="App">
-        <WeatherComponent />
+        <div>
+          <WeatherComponent />
+        </div>
+        <div>
+        <WeatherCards API_Key={API_Key} city="Lahore"/>
+          {/* {
+            citiesState.map((elem) => {
+              return <WeatherCards API_Key={API_Key} city={elem} key={elem}/>
+            })
+          } */}
+        </div>
       </div>
     );
 }
